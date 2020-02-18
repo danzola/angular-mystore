f52d4f4204d6e9d49e0d125c25a46d5f9e0efe5b
danzola <diego_anzola@yahoo.es>
2020-02-18 11:14:39 -0500

    4 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
@@ -25,7 +26,8 @@ import { HeroesComponent } from './heroes/heroes.component';
       { path: 'cart', component: CartComponent },
       { path: 'shipping', component: ShippingComponent },
       { path: 'heroes', component: HeroesComponent },
-    ])
+    ]),
+    FormsModule
   ],
   declarations: [
     AppComponent,
